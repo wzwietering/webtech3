c97fe4dc73b0ca33ff1ccbb57e0f5f4d0fd5f028
Wilmer Zwietering <wzwietering@gmail.com>
2017-03-29 15:01:39 +0200

    Fixed backend

--- back-end/controllers/404.js
+++
@@ -1,5 +0,0 @@
-exports.get = function(req, res) {
-  res.writeHead(404, {'Content-Type': 'text/html'});
-  res.write('<h1>404 - Page not found</h1><p><a href="/">Return to home</a></p>');
-  res.end();
-};
--- back-end/router.js
+++ back-end/router.js
@@ -1,25 +1,36 @@
 var url = require('url');
+var http  = require('http');
 var fs = require('fs');
+
+var types = {
+    html: 'text/html',
+    txt: 'text/plain',
+    css: 'text/css',
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    svg: 'image/svg+xml',
+    js: 'application/javascript'
+};
 
 exports.get = function(req, response) {
   req.requrl = url.parse(req.url, true);
-  var path = req.requrl.pathname;
-  if (/.(css)$/.test(path)) {
-    response.writeHead(200, {
-      'Content-Type': 'text/css'
-    });
-    fs.readFile(__dirname + path, 'utf8', function(err, data) {
-      if (err){
-        throw err;
-      }
+  var pathname = url.parse(req.url).pathname;
+  //console.log("Request for " + pathname + " received.");
+  if(pathname == "/"){
+    pathname = "/index.html";
+  }
+  var elements = pathname.split('.');
+  var type = types[elements[elements.length - 1]] || 'text/plain';
+  fs.readFile('../front-end' + pathname, function(error, data) {
+    if(error){
+      response.writeHead(404, {'Content-Type': 'text/html'});
+      response.write('<h1>404 - Page not found</h1><p><a href="/">Return to home</a></p>');
+      response.end();
+    } else {
+      response.setHeader('Content-Type', type);
       response.write(data, 'utf8');
       response.end();
-    });
-  } else {
-    if (path === '/') {
-      require('./controllers/homepage.js').get(require, response);
-    } else {
-      require('./controllers/404.js').get(require, response);
     }
-  }
+  });
 };
