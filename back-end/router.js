8f79a8377577c2f4002d2ca957d9eca54d11da9c
Wilmer Zwietering <wzwietering@gmail.com>
2017-03-29 15:12:58 +0200

    routing modification

--- back-end/router.js
+++ back-end/router.js
@@ -17,7 +17,7 @@ exports.get = function(req, response) {
   req.requrl = url.parse(req.url, true);
   var pathname = url.parse(req.url).pathname;
   //console.log("Request for " + pathname + " received.");
-  if(pathname == "/"){
+  if(pathname == "/" || "group6/"){
     pathname = "/index.html";
   }
   var elements = pathname.split('.');
